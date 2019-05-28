062fc5078096a6fe2371b5cac6cc793ffee0ce9c
kenkuts <kennethchoi19@gmail.com>
2019-05-28 00:22:48 -0400

    initialized project

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+cure = Artist.create(name: "The Cure", bio: "Goth guys from the UK")
+new_wave = Genre.create(name: "New Wave")
+
+bdc = Song.create(name: "Boy's Don't Cry")
+lull = Song.create(name: "Lullaby")
+trust = Song.create(name: "Trust")
+
+bdc.update(artist_id: cure.id, genre_id: new_wave.id)
+lull.update(artist_id: cure.id, genre_id: new_wave.id)
+trust.update(artist_id: cure.id, genre_id: new_wave.id)
