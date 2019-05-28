94a844dfda349ee6ba34842d8c58a08dbfa95a0a
kenkuts <kennethchoi19@gmail.com>
2019-05-28 03:07:36 -0400

    added more

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-cure = Artist.create(name: "The Cure", bio: "Goth guys from the UK")
-new_wave = Genre.create(name: "New Wave")
+# cure = Artist.create(name: "The Cure", bio: "Goth guys from the UK")
+# new_wave = Genre.create(name: "New Wave")
 
-bdc = Song.create(name: "Boy's Don't Cry")
-lull = Song.create(name: "Lullaby")
-trust = Song.create(name: "Trust")
+# bdc = Song.create(name: "Boy's Don't Cry")
+# lull = Song.create(name: "Lullaby")
+# trust = Song.create(name: "Trust")
 
-bdc.update(artist_id: cure.id, genre_id: new_wave.id)
-lull.update(artist_id: cure.id, genre_id: new_wave.id)
-trust.update(artist_id: cure.id, genre_id: new_wave.id)
+# bdc.update(artist_id: cure.id, genre_id: new_wave.id)
+# lull.update(artist_id: cure.id, genre_id: new_wave.id)
+# trust.update(artist_id: cure.id, genre_id: new_wave.id)
+
+Artist.create(name: "311", bio:"cool")
+Artist.create(name: "The Pixies", bio: "Boston, MA")
+Artist.create(name: "The Smiths", bio: "UK")
+Artist.create(name: "Coheed and Cambria", bio: "Nyack, NY")
